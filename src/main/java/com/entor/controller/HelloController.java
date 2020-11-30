0bdec7f759de6a2f19e4253dff93d7971d2bebd1
QQ <842552709@qq.com>
2020-11-30 16:29:15 +0800

    服务器端口

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${server.port}")
+    private int port;
 
     @RequestMapping("/hello")
     public String hello(){
